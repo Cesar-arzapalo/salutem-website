0375048d9bbd7868103bc7c6fcb709375f470f3b
Cesar-arzapalo <cesar.arzapalo@unmsm.edu.pe>
2021-01-31 14:54:39 -0500

    agregando estilos extras y la pagina de resultado

--- src/app/triaje/resultado/resultado.component.ts
+++ src/app/triaje/resultado/resultado.component.ts
@@ -1,4 +1,6 @@
+
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router'
 
 @Component({
   selector: 'app-resultado',
@@ -6,10 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./resultado.component.css']
 })
 export class ResultadoComponent implements OnInit {
+  cantidad: number;
+  nivelTriaje: number;
+  mensaje: string[];
 
-  constructor() { }
+  constructor(private router: ActivatedRoute) {
+    this.cantidad=0;
+    this.nivelTriaje=1;
+    this.mensaje=['Eres una persona que posee SARS-CoV2, requieres de atención inmediata. tu condición clínica representa un riesgo vital',
+      'Eres una persona que posee SARS-CoV2, tu condicion puede evolucionar hacia un rápido deterioro, nesecitas atencion medica urgente',
+      'Eres un posible caso de SARS-CoV2, tu condición clínica requiere de medidas diagnósticas urgente para descartar',
+      'Eres un posible caso de SARS-CoV2, pero tu condición clínica no requiere de medidas diagnósticas urgente para descartarlo',
+      'No eres un posible caso de SARS-CoV2, no es necesario que hagas un descarte'];
+  }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.cantidad = this.router.snapshot.params.id;
+    this.definirNivel(this.cantidad);
+  }
+
+  definirNivel(minutos: number){
+    if (minutos <= 15){
+      this.nivelTriaje = 1;
+    }else if (minutos <= 60){
+      this.nivelTriaje = 2;
+    }else if (minutos <= 120){
+      this.nivelTriaje = 3;
+    }else if (minutos <= 240){
+      this.nivelTriaje = 4;
+    }else {
+      this.nivelTriaje = 5;
+    }
+
   }
 
 }
+
