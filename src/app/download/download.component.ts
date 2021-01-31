ecf418909900f50caf722ae1b322b2254be224cf
Cesar-arzapalo <cesar.arzapalo@unmsm.edu.pe>
2021-01-31 12:43:15 -0500

    cargando modificaciones en los estilos en download component

--- src/app/download/download.component.ts
+++ src/app/download/download.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-download',
   templateUrl: './download.component.html',
   styleUrls: ['./download.component.css']
+  
 })
 export class DownloadComponent implements OnInit {
 
